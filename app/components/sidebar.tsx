26fffb1d57ca924cb899470b131d4c8ef538b600
pinchao <cpc4253@163.com>
2023-09-10 10:33:40 +0800

    change sidebar titles

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Change World
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Your AI assistant by your side.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
